dfd53df12e1da55570ed13686f3e1c3b16b15cb0
OmarAmeer96 <omar.ameer244@gmail.com>
2024-11-23 19:34:48 +0200

    Add animated splash screen and update dependencies

--- lib/Features/splash/views/splash_view.dart
+++ lib/Features/splash/views/splash_view.dart
@@ -1,14 +1,22 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:ui_challenge/Features/home/views/home_view.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Splash Screen"),
-      ),
+    return AnimatedSplashScreen(
+      splash: Lottie.asset('assets/json/car_tyre_animation.json'),
+      backgroundColor: Colors.white,
+      nextScreen: HomeView(),
+      splashIconSize: 300,
+      duration: 3000,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.rotate,
     );
   }
 }
